82f84138262240fe5544cb5c434abcdc41d11d89
Philip Kurian <philip.j.kurian@gmail.com>
2013-02-26 09:21:57 +1100

    Add titles to static pages.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,13 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                      :text => 'Ruby on Rails Tutorial Sample App | Home')
+    end
+
   end
 
   describe "Help page" do
@@ -15,12 +22,37 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => 'Ruby on Rails Tutorial Sample App | Help')
+    end
   end
 
   describe "About page" do
     it "should have the content 'About'" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                        :text => 'Ruby on Rails Tutorial Sample App | About')
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                                :text => 'Ruby on Rails Tutorial Sample App | Contact us')
     end
   end
 
